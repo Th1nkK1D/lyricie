3a83cee1f2d7538467d2d106addbc1237bd593c2
Withee Poositasai <witheep@gmail.com>
2018-11-06 16:56:29 +0700

    Add axios, main web UI

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,20 @@
 <template>
   <div id="app">
-    Heelo world
+    <div class="container">
+      <div class="title">Riff-Off Bot</div>
+      <div class="cardbox">
+        <div class="chat">
+          chat here
+        </div>
+        <div class="form">
+          <input type="text" />
+          <button>Sing!</button>
+        </div>
+      </div>
+      <div class="footer">
+        Lyrics powered by musiXmatch - Developed by Th1nkK1D 2018 - View on Github
+      </div>
+    </div>
   </div>
 </template>
 
@@ -14,5 +28,82 @@ export default {
 body {
   margin: 0;
   padding: 0;
+  font-family: 'Dosis', sans-serif;
+}
+
+#app {
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  background-color: #FBDA61;
+  background-image: linear-gradient(45deg, #FBDA61 0%, #FF5ACD 100%);
+}
+
+.title {
+  text-align: center;
+  color: white;
+  font-family: 'Pattaya', sans-serif;
+  font-size: 3rem;
+  margin-bottom: 1rem;
+}
+
+.footer {
+  color: white;
+  text-align: center;
+  margin-top: 1rem;
+}
+
+.container {
+  width: 100%;
+  max-width: 600px;
+  height: 85vh;
+  margin: auto;
+  display: flex;
+  flex-direction: column;
+}
+
+.cardbox {
+  background-color: white;
+  border-radius: 10px;
+  height: 100%;
+  box-shadow: 2px 2px 20px rgba($color: #000000, $alpha: 0.3);
+  display: flex;
+  flex-direction: column;
+  overflow: hidden;
+  padding: 0.5rem;
+}
+
+.chat {
+  display: flex;
+  flex: 1;
+  overflow-y: scroll;
+}
+
+.form {
+  display: flex;
+
+  input {
+    display: flex;
+    flex: 1;
+    font-family: 'Dosis', sans-serif;
+    font-size: 1rem;
+    padding: 0.5rem;
+    border-radius: 8px 0 0 8px;
+    border: 1px solid lightgray;
+
+    &:focus {
+      outline-width: 0;
+    }
+  }
+
+  button {
+    font-family: 'Pattaya', sans-serif;
+    font-size: 1rem;
+    background-color: #039be5;
+    color: white;
+    border: none;
+    border-radius: 0 8px 8px 0;
+  }
 }
 </style>
